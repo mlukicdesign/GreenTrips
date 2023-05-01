920dd1a1a81fd671695623e3a6f68037967d3978
ChristianAlteri <alteri.christian@gmail.com>
2023-05-01 20:40:10 +0800

    upload

--- carbon.js
+++ carbon.js
@@ -3,8 +3,8 @@ fetch("https://www.carboninterface.com/api/v1/vehicle_makes", {
   method: "GET",
   headers: {
     Authorization: "Bearer 6QeGE5VqAKrw1NYnUNp8Q",
-    "Content-Type": "application/json"
-  },
+    "Content-Type": "application/json",
+  }
 })
   .then(function(response) {
     return response.json();
@@ -13,7 +13,8 @@ fetch("https://www.carboninterface.com/api/v1/vehicle_makes", {
     // Store the IDs of the vehicle makes
     data.forEach(function(data) {
       cars.push(data.data.attributes.name);
-      // console.log(data.data.attributes.name);
+      console.log(data);
+      console.log(data.data.attributes.name);
       localStorage.setItem("cars", JSON.stringify(cars));
     });
   })
