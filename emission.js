108a8b11edcc271ec33c4b5791dd0cae71940d24
ChristianAlteri <alteri.christian@gmail.com>
2023-05-08 12:35:55 +0800

    -Cleaned JS-Only thing left for JS is to add interaction with stored journeys. just need to re display the old journeys to the DOM-wrote conditionals for null or invalid inputs.

--- emission.js
+++
@@ -1,9 +0,0 @@
-// handle emission calculation after clicking calculate button
-
-document.addEventListener("load", function (){
-    // add event listener calc button
-    // fisrt call google maps api
-    initMap();
-    // then carbon api
-    
-})
